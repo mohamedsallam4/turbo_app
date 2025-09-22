cf42d86b3bf7ff69dd86c941914c31cd7c2a4735
mohamedsallam4 <mohamedsallam20045@gmail.com>
2025-09-23 01:43:51 +0300

    "new#6"

--- lib/fetures/auth/modules/login/login_screen.dart
+++ lib/fetures/auth/modules/login/login_screen.dart
@@ -169,6 +169,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ],
               ),
+              SizedBox(height: 10,)
             ],
           ),
         ),
